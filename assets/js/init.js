a85154c720867014f128fcebcac12c0e6886551b
rf <rf.fa17@gmail.com>
2019-12-05 14:49:42 +0330

    materialize framework assets and slider and head include files added

---
+++ assets/js/init.js
@@ -0,0 +1,20 @@
+(function($){
+  $(function(){
+
+    $('.sidenav').sidenav({
+      edge: 'right',
+      draggable: true,
+    });
+    $('.parallax').parallax();
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
+
+
+// Slider
+$(document).ready(function(){
+  $('.slider').slider({
+    indicators: true,
+    height:400,
+  });
+});
