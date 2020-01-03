4b0b7f43e2900ddf99a8c1fb428c5cdea44a517e
rf <rf.fa17@gmail.com>
2020-01-03 17:17:47 +0330

    owl carousel added to home header and some other changes

--- assets/js/init.js
+++ assets/js/init.js
@@ -88,7 +88,7 @@ $(document).ready(function(){
     nav:false,
 
     autoplay:true,
-    autoplayTimeout:4000,
+    autoplayTimeout:5000,
     autoplayHoverPause:true,
 
     responsive:{
@@ -96,10 +96,10 @@ $(document).ready(function(){
             items:1
         },
         600:{
-            items:2
+            items:1
         },
         1000:{
-            items:3
+            items:1
         }
     }
 })
